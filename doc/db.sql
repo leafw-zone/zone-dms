3b4cdff3000025737329834b1c14bfe21eeef930
卫宫 <wuyeran@qiandongdong.cn>
2018-07-11 18:06:09 +0800

    db

---
+++ doc/db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `dic_info` (
+  `dic_id` varchar(30) NOT NULL COMMENT '编号',
+  `key` varchar(30) DEFAULT NULL COMMENT '键',
+  `name` varchar(30) DEFAULT NULL COMMENT '值',
+  `parent_id` varchar(30) DEFAULT NULL COMMENT '父键id',
+  `sys_id` varchar(30) DEFAULT NULL COMMENT '系统id',
+  `is_deleted` varchar(1) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `create_by` varchar(30) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `update_by` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`dic_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
